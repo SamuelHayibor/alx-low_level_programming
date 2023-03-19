de5d96bb67bbe5d9bbd30d2c4e2c97dd17312ea2
SamuelHayibor <adomsamuel123@gmail.com>
2023-03-19 01:18:03 +0000

    12. Software is eating the World

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,15 @@
+/*
+* File: 0-positive_or_negative.c
+*
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
-* File:: 0-positive_or_negative.c
-*
-*/
 
 /**
-* main- print the last digit of the number stored in the variable n
+*main - print the last digit of the number stored in the variable n
 *
-* Return: Always 0.
+*Return: Always 0.
 */
 int main(void)
 {
@@ -20,15 +20,18 @@ n = rand() - RAND_MAX / 2;
 
 if ((n % 10) > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+printf("Last digit of %d is %d and is greater than 5\n",
+n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %d is %d and is less than 6 and not 0\n",
+n, n % 10);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
+printf("Last digit of %d is %d and is 0\n",
+n, n % 10);
 }
 
 return (0);
