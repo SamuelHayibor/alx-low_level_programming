04dd40651d4d84266ba65e6676f8ac8a228105be
SamuelHayibor <adomsamuel123@gmail.com>
2023-03-19 00:46:40 +0000

    12. Software is eating the World

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
 */
 #include <stdio.h>
 /**
-* main -  prints all possible different combinations of two digits.
+* main - prints all possible different combinations of two digits.
 *
 * Return: Always 0.
 */
