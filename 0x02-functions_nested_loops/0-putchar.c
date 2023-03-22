9ec5eb6f90d14bdb3e879a5636558a312b4e6246
SamuelHayibor <adomsamuel123@gmail.com>
2023-03-22 02:18:08 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *main - Entry point
-*
+*Return: Always 0
 */
 int main(void)
 {
@@ -14,5 +14,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
